21b4b762dea2231343f6cdf715ff3395c9704ed6
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-19 03:37:21 -0500

    creacion y estilado

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
+function App() {
+  return (
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} variant="h3" component="h1">
+          Buscador de Noticias
+        </Typography>
+      </header>
+      <Formulario />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Box,
+} from "@mui/material";
+//
+//
+const CATEGORIAS = [
+  { value: "general", label: "General" },
+  { value: "business", label: "Negocios" },
+  { value: "entertainment", label: "Entretenimiento" },
+  { value: "health", label: "Salud" },
+  { value: "science", label: "Ciencia" },
+  { value: "sports", label: "Deportes" },
+  { value: "technology", label: "Tecnología" },
+];
+//
+//
+//
+
+const Formulario = () => {
+  return (
+    <form>
+      <FormControl>
+        <InputLabel>Categoria</InputLabel>
+        <Select label="categoria"></Select>
+      </FormControl>
+    </form>
+  );
+};
+
+export default Formulario;
